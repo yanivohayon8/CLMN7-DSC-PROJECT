066da843f1efc34ca8cd228e566f72fa3469d671
Ruti <rutikevesh@gmail.com>
2020-04-29 17:20:45 +0300

    made it possible to choose the power threshold from outside the function in order to optimize it

--- src/models/audio.py
+++ src/models/audio.py
@@ -14,7 +14,7 @@ def getSubjSwitchPoints(wavFilePath):
     
 
 
-# In[ ]:
+# In[78]:
 
 
 from __future__ import unicode_literals
@@ -38,10 +38,10 @@ ydl_opts = {
 }
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=Akwm2UZJ34o'])
+    ydl.download(['https://www.youtube.com/watch?v=MkiUBJcgdUY'])
 
 
-# In[7]:
+# In[80]:
 
 
 # Import the AudioSegment class for processing audio and the 
@@ -56,11 +56,11 @@ def match_target_amplitude(aChunk, target_dBFS):
    return aChunk.apply_gain(change_in_dBFS)
 
 # Load your audio.
-audio = AudioSegment.from_wav("Hot Food _ POEM _ The Hypnotiser _ Kids' Poems and Stories With Michael Rosen-Akwm2UZJ34o.wav")
+audio = AudioSegment.from_wav("Mod-01 Lec-01 Foundation of Scientific Computing-01-MkiUBJcgdUY.wav")
 print(audio)
 
 
-# In[21]:
+# In[81]:
 
 
 def db_to_float(db, using_amplitude=True):
@@ -75,7 +75,7 @@ def db_to_float(db, using_amplitude=True):
         return 10 ** (db / 10)
 
 
-# In[37]:
+# In[82]:
 
 
 audio_len = len(audio) # result is in ms
@@ -148,13 +148,13 @@ for idx,silence_range in enumerate(silence_ranges_len):
 print(subject_switch_silent_ranges)
 
 
-# In[76]:
+# In[83]:
 
 
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-def getSubjSwitchPoints(wavFilePath):
+def getSubjSwitchPoints(wavFilePath , silence_threshold):
     switchPoints = []
     audio = AudioSegment.from_wav(wavFilePath)
     audio_len = len(audio) # result is in ms
@@ -167,7 +167,7 @@ def getSubjSwitchPoints(wavFilePath):
     last_slice_start_point = audio_len - SLICE_LEN
     slice_start_points = range(0, last_slice_start_point + 1, 1)
 
-    silence_thresh = -30 # silence threshold
+    silence_thresh = silence_threshold # silence threshold
 
     # convert silence threshold to a float value (so we can compare it to rms)
     silence_thresh = db_to_float(silence_thresh) * audio.max_possible_amplitude
@@ -243,13 +243,39 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     
 
 
-# In[77]:
+# In[86]:
 
 
-silent_ranges = getSubjSwitchPoints("Hot Food _ POEM _ The Hypnotiser _ Kids' Poems and Stories With Michael Rosen-Akwm2UZJ34o.wav")
+silent_ranges = getSubjSwitchPoints("Mod-01 Lec-01 Foundation of Scientific Computing-01-MkiUBJcgdUY.wav")
 
 
+
+print(silence_ranges_len)
+
+
+# In[90]:
+
+
+silence_ranges_len= list(map(lambda silence_range : silence_range[1]-silence_range[0] ,silent_ranges))
 print(silent_ranges)
+print(silence_ranges_len)
+
+
+# In[93]:
+
+
+import matplotlib.pyplot as plt
+
+n, bins, patches = plt.hist(x=silence_ranges_len, bins='auto', color='#0504aa',
+                            alpha=0.7, rwidth=0.85)
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('ms')
+plt.ylabel('quantity')
+plt.title('My Very Own Histogram')
+plt.text(23, 45, r'$\mu=15, b=3$')
+maxfreq = n.max()
+# Set a clean upper y-axis limit.
+plt.ylim(ymax=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
 
 
 # In[ ]:
