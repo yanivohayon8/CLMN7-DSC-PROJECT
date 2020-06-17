a57b945532595f70cb4d2ece2c9d5b5fac8454e8
yanivohayon8 <yanivohayon8@gmail.com>
2020-06-17 11:26:36 +0300

    fixing reading statbook docx

--- src/data/docx.py
+++ src/data/docx.py
@@ -145,55 +145,72 @@ def find_content_statbook(full_text,font_sizes,main_chapter_keyword = 'Topic'):
     p_Start_Section = re.compile('{} \d?'.format(main_chapter_keyword))
     p_subsection_numbering = re.compile(r'^((\d+\.)+\d*)$')
     p_words = re.compile('[A-Za-z]+')
-    total_corpus = []
-    chapter_corpus = ""
     
-    for i,(doc_text,doc_size) in enumerate(zip(full_text,font_sizes)):
-        ''' if it is a header '''
-        #print(doc_size)
-        #print(most_common_font_size)
-        if(doc_size > most_common_font_size):
-            is_title = False
-            matching_start_chapter = p_Start_Section.match(doc_text)       
-            # if it is start of new chapter\main topic
-            if matching_start_chapter is not None:
-                topic_titles.append(full_text[i+1])
-                #print("main chapter %s size %s " %(full_text[i+1],font_sizes[i+1]))
-                main_chapter_titles.append(full_text[i+1])
-                is_title = True
-            else:
-                # if it is a start of sub topic
-                '''comes from the structure in the input array as:
-                [
-                    1.1
-                    Types of Data
-                ]
-                
-                '''
-                matching_start_section_numbering = p_subsection_numbering.match(doc_text)    
-                matching_words = p_words.match(full_text[i+1])
-                if matching_start_section_numbering is not None and matching_words is not None:
-                    
-                    # the subsection annotation (15.1 etc)
-                    # must be at the same size as the label (Introduction etc)
-                    if most_common_font_size < font_sizes[i+1]:
-                        sub_section = ("%s %s" %(full_text[i],full_text[i+1]))
-                        #print("subsection %s size %s %s" %(sub_section,font_sizes[i],font_sizes[i+1]))
-                    else:
-                        sub_section = full_text[i]
-                        #print("subsection %s size %s" %(sub_section,font_sizes[i]))
-                    is_title = True
-                    
-                    topic_titles.append(sub_section)
-            if is_title:
-                total_corpus.append(chapter_corpus[1:])
-                chapter_corpus = ""
+    main_chapter_indexes = [i+1 for i in range(len(full_text) - 1)\
+                            if p_Start_Section.match(full_text[i]) is not None and font_sizes[i] > most_common_font_size]
+    sub_chapter_indexes  = [i for i in range(len(full_text))\
+                            if p_subsection_numbering.match(full_text[i]) is not None
+                            and
+                            font_sizes[i] > most_common_font_size]
+    all_titles_indexes = main_chapter_indexes + sub_chapter_indexes    
+    all_titles_indexes.sort()
+    
+    
+    '''Finding titles values'''
+    topic_titles  = []
+    for j in all_titles_indexes:
+        # subsection
+        if j not in main_chapter_indexes:
+           # if no label is given while reading the doc
+           # ..
+           # 2.1.2
+           # my corpus and my content
+           if '.' in full_text[j + 1] or ',' in full_text[j + 1]:
+               topic_titles.append(full_text[j])
+           else:
+               # 1.1
+               # introduction
+               topic_titles.append("%s %s" %(full_text[j],full_text[j+1]))
+        else:
+            # main chapter
+            topic_titles.append(full_text[j])
+
+    main_chapter_titles = [full_text[j] for j in main_chapter_indexes]
+
+    
+    
+    total_corpus  = []
+    for i in range(len(all_titles_indexes) - 1):
+        sec_as_text = reduce(lambda acc,x: acc+" " +x,
+                             full_text[all_titles_indexes[i]:all_titles_indexes[i+1]],"")
+        # adding dot for finishing the corpus
+        if len(sec_as_text) == 0:
+            pass
+            #sec_as_text = 'section empty.' # planting plain text for non empty
+        elif sec_as_text[-1] != '.':
+            sec_as_text = sec_as_text + '.'
+        total_corpus.append(sec_as_text)
         
-        elif doc_size == most_common_font_size:
-            chapter_corpus = chapter_corpus + ' ' + doc_text
     
-  
+    # adding the text of the last section
+    last_sec_text = ""
+    i = all_titles_indexes[-1] + 1
+    while i < len(full_text) and font_sizes[i] <= most_common_font_size:
+        last_sec_text = last_sec_text + " " + full_text[i]
+        i+=1
+    total_corpus.append(last_sec_text)
+    
+    
+    # fill empty subssections with random string
+     
     total_corpus = process_docx(total_corpus)
+    
+    for i in range(len(total_corpus)):
+        if len(total_corpus[i]) == 0:
+            total_corpus[i] = [['section','empty']]
+    
+    
+    
     return {'corpus':total_corpus,'titles':topic_titles,'main titles':main_chapter_titles}
     
 
